c5ff300bba215af99c6a5d361ee3d4d70d4d0237
atnbn <86778371+atnbn@users.noreply.github.com>
2022-05-21 10:42:36 +0200

    import dialog

--- src/app/dialog-add-player/dialog-add-player.component.ts
+++ src/app/dialog-add-player/dialog-add-player.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 
 @Component({
   selector: 'app-dialog-add-player',
   templateUrl: './dialog-add-player.component.html',
-  styleUrls: ['./dialog-add-player.component.scss']
-
+  styleUrls: ['./dialog-add-player.component.scss'],
+  imports: [MatInputModule]
 })
+
+
+
 export class DialogAddPlayerComponent implements OnInit {
   name:string = ''
   constructor() { }
