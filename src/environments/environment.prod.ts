3c27117c3e611ccb9e6cd3f7dee08a679a0abbc5
atnbn <86778371+atnbn@users.noreply.github.com>
2022-06-03 09:23:23 +0200

    adding firebase deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,14 @@
 export const environment = {
-  
-    firebase: {
-      projectId: 'ring-of-fire-2c176',
-      appId: '1:241672961574:web:fa6706d9ded3c0c04630e7',
-      storageBucket: 'ring-of-fire-2c176.appspot.com',
-      apiKey: 'AIzaSyB_nxTyc6ROq1zBInr7ftyMPPaHekSi6MY',
-      authDomain: 'ring-of-fire-2c176.firebaseapp.com',
-      messagingSenderId: '241672961574',
-      measurementId: 'G-S20B534F83',
+  firebase: {
+    projectId: 'ring-of-fire-2c176',
+    appId: '1:241672961574:web:fa6706d9ded3c0c04630e7',
+    databaseURL: 'https://ring-of-fire-2c176-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'ring-of-fire-2c176.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyB_nxTyc6ROq1zBInr7ftyMPPaHekSi6MY',
+    authDomain: 'ring-of-fire-2c176.firebaseapp.com',
+    messagingSenderId: '241672961574',
+    measurementId: 'G-S20B534F83',
   },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,9 @@ export const environment = {
   firebase: {
     projectId: 'ring-of-fire-2c176',
     appId: '1:241672961574:web:fa6706d9ded3c0c04630e7',
+    databaseURL: 'https://ring-of-fire-2c176-default-rtdb.europe-west1.firebasedatabase.app',
     storageBucket: 'ring-of-fire-2c176.appspot.com',
+    locationId: 'us-central',
     apiKey: 'AIzaSyB_nxTyc6ROq1zBInr7ftyMPPaHekSi6MY',
     authDomain: 'ring-of-fire-2c176.firebaseapp.com',
     messagingSenderId: '241672961574',
